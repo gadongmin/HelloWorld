87de6ae109d3baa54a1849bf88d060f6de7576a5
gadongmin <dmkim777@naver.com>
2025-04-17 11:48:15 +0900

    20250417(1)

---
+++ src/com/javaex/hello/HelloWorid.java
@@ -0,0 +1,22 @@
+/*
+작성자: 김동민
+작성일: 2024-04-14
+설 명: Hello World 출력하기
+*/
+
+//경로표시
+package com.javaex.hello;
+
+//클래스 제목
+public class HelloWorid {
+
+
+    public static void main(String[] args){
+
+    	//문자열을 화면에 출력하는 함수
+    	System.out.println("Hello World!");
+    	
+    	//git 버전만들기 테스트
+    	   	
+    }
+}
